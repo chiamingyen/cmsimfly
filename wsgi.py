07cec227d6075da013c3abf169126f27d2a8b0ed
scrum-1 <scrum1@mde.tw>
2017-01-25 20:48:07 +0800

    add local https to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,12 +3,16 @@
 import os
 # 導入同目錄下的 myflaskapp.py
 import flaskapp
-    
+import ssl
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('localhost.crt', 'localhost.key')
+
 # 以下開始判斷在 OpenShift 或近端執行
 if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
     # 表示程式在雲端執行
     application = flaskapp.app
 else:
     # 表示在近端執行, 以 python3 wsgi.py 執行,  若採 uwsgi 則與 Openshift 運作模式相同
-    flaskapp.app.run(debug=True)
+    flaskapp.app.run(host='127.0.0.1', port=9443, debug=True, ssl_context=context)
 
